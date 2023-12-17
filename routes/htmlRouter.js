e1911d3e8db5eede97197b48bec18374a8531b8e
justinsta624 <justinsta624@gmail.com>
2023-12-17 17:00:20 -0500

    add Router.js files

--- routes/htmlRouter.js
+++ routes/htmlRouter.js
@@ -1,4 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 
+// get request
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
 module.exports = router;
